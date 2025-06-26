acb1689c08df100ef1e8d0a514b79bf096f01026
AliAbdur1 <AliAbdur171@gmail.com>
2025-06-26 01:48:02 -0400

    camera stuff

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,32 @@
 import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import gsap from 'gsap';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+};
+
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const cursor = {
+  x: 0,
+  y: 0
+}
+
+window.addEventListener('mousemove', (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = - (event.clientY / sizes.height - 0.5);
+  // console.log(`cursor x position is ${cursor.x}, cursor y position is ${cursor.y}`);
+});
 function Square() {
   const canvasRef = useRef(null);
 
@@ -13,8 +38,8 @@ function Square() {
     const cube = new THREE.Mesh(geometry, material);
 
     const group = new THREE.Group();
-    group.position.y = 1;
-    group.scale.y = 2;
+    group.position.y = 0;
+    group.scale.y = 1;
     group.rotation.y = Math.PI / 4;
     scene.add(group);
 
@@ -22,7 +47,7 @@ function Square() {
       new THREE.BoxGeometry(1, 1, 1),
       new THREE.MeshBasicMaterial({ color: 0x98F5F9 }) // applys material to the cube
     )
-    cube1.position.x = 1;
+    cube1.position.x = 0;
 
     
 
@@ -30,7 +55,7 @@ function Square() {
       new THREE.BoxGeometry(1, 1, 1),
       new THREE.MeshBasicMaterial({ color: 0xFFECA1 }) // applys material to the cube
     )
-    cube2.position.x = -2;
+    cube2.position.x = -1.5;
 
     
 
@@ -38,13 +63,11 @@ function Square() {
       new THREE.BoxGeometry(1, 1, 1),
       new THREE.MeshBasicMaterial({ color: 0xEFC3CA }) // applys material to the cube
     )
-    cube2.position.x = 2.5;
+    cube3.position.x = 1.5;
 
     group.add(cube1, cube2, cube3); // adds the cube to the group
     // position properties of the cube. must be before reder call
-    // cube.position.x = -4;
-    // cube.position.y = -3;
-    // cube.position.z = -2;
+  
     scene.add(cube);
     cube.position.set(-2, -3, -4); // same as above, lets u set all at once
     cube.scale.set(2, 0.5, .5);
@@ -61,16 +84,22 @@ function Square() {
     scene.add(axesHelper); // axishelper is an object and must be added to the scene
 
     
-    const sizes = {
-      width: 800,
-      height: 600
-    };
     
-    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-    camera.position.set (-1, -1, 5); // same as above, lets u set all camera positions at once
+    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000); // field of veiw recommendation 45 - 75. 1 and 1000 are near and far perameters
+    const aspectRatio = sizes.width / sizes.height;
+    // const camera = new THREE.OrthographicCamera(
+    //   -1 * aspectRatio,
+    //    1 * aspectRatio,
+    //     1, 
+    //     -1, 
+    //     0.1, 
+    //     100);
+    camera.position.set (-1, 1, 6); // same as above, lets u set all camera positions at once
     console.log(cube.position.distanceTo(camera.position),"camera position");
     scene.add(camera);
 
+    const controls = new OrbitControls(camera, canvasRef.current)
+    controls.enableDamping = true;
     camera.lookAt(group.position);
 
     const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
@@ -82,21 +111,27 @@ function Square() {
     gsap.to(cube.position, { x: 4, duration: 1, delay: 1})
     gsap.to(cube.position, { x: 0, duration: 1, delay: 3})
     // green sock or gsap has its own tick
+
+    // Controls
     const tick = () => {
       const elapsedTime = clock.getElapsedTime();
 
       // const currenttime = Date.now();
       // const deltaTime = currenttime - time;
       // time = currenttime;
+
+      // camera.position.x = cursor.x * 5;// ties cursor position to camera movement
+      // camera.position.y = cursor.y * 5;// ties cursor position to camera movement
+
       
-      // update objects
 
       // group.rotation.y = elapsedTime * Math.PI * 2; // deltaTime; // make objects rotate at same speed regardless of frame rate
-      // group.position.x = elapsedTime * Math.sin(elapsedTime) / 3
-      // group.position.y = elapsedTime * Math.cos(elapsedTime) / 3
-      camera.position.y = Math.sin(elapsedTime);
-      camera.position.x = Math.cos(elapsedTime);
-      camera.lookAt(group.position);
+      // group.rotation.x = elapsedTime * Math.sin(elapsedTime)
+      // group.rotation.y = elapsedTime * Math.cos(elapsedTime)
+      // camera.position.y = Math.sin(elapsedTime);
+      // camera.position.x = Math.cos(elapsedTime);
+      
+      // camera.lookAt(group.position);
       // let cubeRotation = cube.rotation.x;
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
@@ -106,7 +141,7 @@ function Square() {
       // while (cubeRotation < 1) {
       //   cubeRotation += 0.02;
       // }
-      
+      controls.update()
       
       renderer.render(scene, camera);
       window.requestAnimationFrame(tick); // calls the function again
